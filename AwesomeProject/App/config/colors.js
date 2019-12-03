35563b5c05305aab459ea3a8408f21c821c5b098
lalitbehera <lalitkumar.b@landmarkgroup.in>
2019-12-03 17:48:40 +0530

    building components using storybook

---
+++ AwesomeProject/App/config/colors.js
@@ -0,0 +1,7 @@
+export default {
+    primary: "#202c41",
+    border: "#c6c6c6",
+    white: "#fff",
+    gray: "#b55464",
+    success: "#50c356"
+}
---
+++ AwesomeProject/App/index.js
@@ -0,0 +1,33 @@
+
+export default from '../storybook';
+
+//import { from } from "rxjs";
+//
+//import { from } from "rxjs";
+
+// import React from 'react';
+// import {
+//   SafeAreaView,
+//   StyleSheet,
+//   ScrollView,
+//   View,
+//   Text,
+//   StatusBar,
+// } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
