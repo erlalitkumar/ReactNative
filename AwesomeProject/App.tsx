06a6090207086fb07e97b5739b085aa8ef8c98b3
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-01-09 20:33:20 +0530

    base -typescript react native project

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,35 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import Hello from './components/hello';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {StyleSheet, View, Text} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Hello name={'Lalit'} enthusiasmLevel={6} />
+        <Text>Hello world</Text>
       </View>
     );
   }
